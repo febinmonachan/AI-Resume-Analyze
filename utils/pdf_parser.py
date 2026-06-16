35515641e01445e4f653758c9e956c61198d25ac
febin monachan <febinm06@gmail.com>
2026-06-16 11:50:28 +0530

    Update pdf_parser.py

--- utils/pdf_parser.py
+++ utils/pdf_parser.py
@@ -1,1 +1,14 @@
+import pdfplumber
+import io
 
+def extract_text_from_pdf(uploaded_file):
+    text = ""
+
+    with pdfplumber.open(
+        io.BytesIO(uploaded_file.read())
+    ) as pdf:
+
+        for page in pdf.pages:
+            text += page.extract_text() or ""
+
+    return text.strip()

b4c1a2bc5092ec1ddfe258d5dc92f785b9bffad2
febin monachan <febinm06@gmail.com>
2026-06-16 11:51:18 +0530

    Update json_parser.py

--- utils/json_parser.py
+++ utils/json_parser.py
@@ -1,1 +1,32 @@
+import json
+import re
 
+def safe_parse_json(text):
+
+    text = re.sub(r"```json", "", text)
+    text = re.sub(r"```", "", text)
+
+    text = text.strip()
+
+    try:
+        return json.loads(text)
+
+    except json.JSONDecodeError:
+
+        match = re.search(
+            r'(\{.*\})',
+            text,
+            re.DOTALL
+        )
+
+        if match:
+
+            try:
+                return json.loads(
+                    match.group(1)
+                )
+
+            except:
+                return None
+
+    return None
